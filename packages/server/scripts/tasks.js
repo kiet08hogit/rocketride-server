b3e43145a8d23b94308ec182430a7fd3361ba1a7
kiet08hogit <hokiet.nguyen08@gmail.com>
2026-08-13 14:47:26 -0400

    revert: drop unrelated rocketlib pytest --rootdir change

--- packages/server/scripts/tasks.js
+++ packages/server/scripts/tasks.js
@@ -1225,7 +1225,7 @@ function makeRocketlibPythonTestAction(options = {}) {
 			const exeExt = isWindows() ? '.exe' : '';
 			const engine = path.join(DIST_DIR, 'engine' + exeExt);
 
-			const extraArgs = ['-v', '--rootdir', path.join(SERVER_DIR, 'engine-lib', 'rocketlib-python')];
+			const extraArgs = ['-v'];
 			if (options.pytest) {
 				const tokens = typeof options.pytest === 'string'
 					? options.pytest.split(/\s+/).filter(Boolean)
